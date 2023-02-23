29f6c4bace43554fc16a8d4b4fe2b1e6b7681605
√ｲﾶﾑム <113216701+magnusekvojtech@users.noreply.github.com>
2023-02-23 22:45:27 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+string retezec = "krokonosohroch";
+Console.WriteLine(retezec.StartsWith("krok"));
+Console.WriteLine(retezec.EndsWith("hroch"));
+Console.WriteLine(retezec.Contains("nos"));
+Console.WriteLine(retezec.Contains("roh"));
+Console.ReadKey();
